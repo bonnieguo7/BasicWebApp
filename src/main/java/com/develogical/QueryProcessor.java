8db4c387855cbcf01b00f2942c89192b864c3bba
bonnieguo7 <bmguo@andrew.cmu.edu>
2022-09-22 10:46:47 -0400

    fixed query processor

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,13 +8,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        return "";
-    }
-
-    public String process(String query) {
+    
         if (query.toLowerCase().contains("what is your name")) {
             return "bonnie";
         }
-        return "";
     }
 }
